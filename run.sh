ac34f975727a12c89e0993cc4396ea0a5fcb15a0
dentych <dennis@tychsen.me>
2017-08-26 19:10:30 +0000

    Added some scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-VERSION=$1
-SAVEFOLDER=$2
-
-docker run --name factorio -it -p 34197:34197/udp -v $SAVEFOLDER:/opt/factorio/savegame dentych/factorio:$VERSION
+docker run --name factorio -it --rm \
+    -p 34197:34197/udp \
+    -v $(pwd)/saves:/opt/factorio/saves \
+    -v $(pwd)/server-settings.json:/opt/factorio/server-settings.json \
+    dentych/factorio:0.15.33 \
+    $@
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. run.sh --start-server savegame.zip --server-settings ../server-settings.json
